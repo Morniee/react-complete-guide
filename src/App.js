63a50136a98f6ed49eb7e46c1f9bb4940f5fee38
Kevin Mornie <kevin.morniee@gmail.com>
2018-01-05 15:51:06 +0100

    Passing method references

--- src/App.js
+++ src/App.js
@@ -15,14 +15,14 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // DON'T DO THIS => this.state.persons[0] = "Sergej"
     //
     // will override the persons in the state.
     this.setState({
       persons: [
         {
-          name: "Sergej",
+          name: newName,
           age: "26"
         }, {
           name: "Niels",
@@ -34,9 +34,17 @@ class App extends Component {
 
   render() {
     return (<div className="App">
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies are programming.</Person>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are football.</Person>
-      <button onClick={this.switchNameHandler}>Switch name</button>
+      <Person
+        name={this.state.persons[0].name}
+        age={this.state.persons[0].age}
+        click={this.switchNameHandler.bind(this, "Sam")}>My hobbies are programming.</Person>
+      <Person
+        name={this.state.persons[1].name}
+        age={this.state.persons[1].age}
+        //call anonymous function which then returns a function call. Now u can pass data easily.
+        //binding is more efficient.
+        click={() => { this.switchNameHandler("Jim")}}>My hobbies are football.</Person>
+      <button onClick={this.switchNameHandler.bind(this, "Sergej")}>Switch name</button>
     </div>);
   }
 }
