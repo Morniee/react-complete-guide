b995c42f59321e894f75ff5065d7527d9dfecb65
Kevin Mornie <kevin.morniee@gmail.com>
2018-01-05 15:36:00 +0100

    manipulating state

--- src/App.js
+++ src/App.js
@@ -14,10 +14,29 @@ class App extends Component {
       }
     ]
   }
+
+  switchNameHandler = () => {
+    // DON'T DO THIS => this.state.persons[0] = "Sergej"
+    //
+    // will override the persons in the state.
+    this.setState({
+      persons: [
+        {
+          name: "Sergej",
+          age: "26"
+        }, {
+          name: "Niels",
+          age: "26"
+        }
+      ]
+    })
+  }
+
   render() {
     return (<div className="App">
       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies are programming.</Person>
       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are football.</Person>
+      <button onClick={this.switchNameHandler}>Switch name</button>
     </div>);
   }
 }
