3240a7be94fe070838b6ff626833ff25e80e639b
Unknown <kevin.morniee@gmail.com>
2018-01-08 10:55:09 +0100

    rendering lists with map

--- src/App.js
+++ src/App.js
@@ -66,17 +66,11 @@ class App extends Component {
     if(this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-            click={this.switchNameHandler.bind(this, "Sam")}
-            changed={this.nameChangedHandler}>My hobbies are programming.</Person>
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            //call anonymous function which then returns a function call. Now u can pass data easily.
-            //binding is more efficient.
-            click={() => { this.switchNameHandler("Jim")}}>My hobbies are football.</Person>
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              age={person.age} />
+          })}
         </div>
       );
     }
