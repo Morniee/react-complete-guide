e123c7f93ab123f62a47a3e76cc4af9e8e4b703e
Unknown <kevin.morniee@gmail.com>
2018-01-10 16:03:38 +0100

    reset Radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import Radium from 'radium';
 import './Person.css';
 
 const person = (props) => {
-  const style = {
-    '@media (min-width: 500px)': {
-      width: '450px'
-    }
-  };
-
   return (
     <div className="Person" style={style}>
       <p onClick={props.clicked}>I'm a {props.name} and I'm {props.age} years old!</p>
@@ -17,4 +10,4 @@ const person = (props) => {
   );
 }
 
-export default Radium(person);
+export default person;
