4ef68f7fca1dbd55e2975fef6ce3c55aa1d2720d
Unknown <kevin.morniee@gmail.com>
2018-01-12 16:13:43 +0100

    Converting stateless to statefull components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Person.css';
 
-const person = (props) => {
-  return (
-    <div className={styles.Person}>
-      <p onClick={props.clicked}>I'm a {props.name} and I'm {props.age} years old!</p>
-      <p>{props.children}</p>
-      <input type='text' onChange={props.changed} value={props.name} />
+class Person extends Component {
+  render() {
+    return <div className={styles.Person}>
+      <p onClick={this.props.clicked}>I'm a {this.props.name} and I'm {this.props.age} years old!</p>
+      <p>{this.props.children}</p>
+      <input type='text' onChange={this.props.changed} value={this.props.name} />
     </div>
-  );
+  }
 }
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    return <Person
-      key={person.id}
-      name={person.name}
-      age={person.age}
-      clicked={() => props.clicked(index)}
-      changed={(event) => props.changed(event, person.id)} />
-  });
+class Persons extends Component {
+  render() {
+    return this.props.persons.map((person, index) => {
+      return <Person
+        key={person.id}
+        name={person.name}
+        age={person.age}
+        clicked={() => this.props.clicked(index)}
+        changed={(event) => this.props.changed(event, person.id)} />
+      });
+    }
+}
 
-  export default persons;
+export default Persons;
